0874dbef71db75b3a5ff58b58f4dc5b4e0d649e6
Joanie Davis <joanie.davis@atomicobject.com>
2019-01-10 16:47:25 -0500

    Update mod with random hair assignment

--- RandomHairColorMod/HairColorMod.cs
+++ RandomHairColorMod/HairColorMod.cs
@@ -8,11 +8,15 @@ namespace RandomHairColorMod
 {
     public class HairColorMod : Mod
     {
+        private Random random;
         /************ Public methods *********/
         /// <summary>The mod entry point, called after the mod is first loaded.</summary>
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
+            int seed = DateTime.Now.Second;
+            random = new Random(seed);
+
             // event += method to call
             helper.Events.GameLoop.DayStarted += this.OnDayStarted;
         }
@@ -22,8 +26,15 @@ namespace RandomHairColorMod
         /// <param name="e">The event arguments.</param>
         private void OnDayStarted(object sender, DayStartedEventArgs e)
         {
-            Game1.textColor = Color.Blue;
-            Game1.textShadowColor = Color.BlueViolet;
+            int r = random.Next() % 256;
+            int g = random.Next() % 256;
+            int b = random.Next() % 256;
+            Color current_color = new Color(r, g, b);
+
+            this.Monitor.Log($"R is {r}.");
+            this.Monitor.Log($"G is {g}.");
+            this.Monitor.Log($"B is {b}.");
+            Game1.player.changeHairColor(current_color);
         }
     }
 }
